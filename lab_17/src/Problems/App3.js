0b975ff4d4824a1c5a4be28c1261015bce3953fd
Andrew Kulchickii <droppet228@gmail.com>
2023-03-20 16:35:37 +0200

    18 lab

--- lab_17/src/Problems/App3.js
+++ lab_17/src/Problems/App3.js
@@ -15,3 +15,12 @@ class App3 extends Component {
 }
 
 export default App3;
+
+db,users.insertMany([
+    {name: "Vasya", age: 28},
+    {name: "Nasta", age: 29},
+    {name: "Asya", age: 27},
+    {name: "Lasya", age: 22},
+    {name: "Masya", age: 25},
+    {name: "Dasya", age: 27},
+])
---
+++ lab_18/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App1 from './Problems/App1';
+import App2 from './Problems/App2';
+import App3 from './Problems/App3';
+import App4 from './Problems/App4';
+import App5 from './Problems/App5';
+import App6 from './Problems/App6';
+import App7 from './Problems/App7';
+import App8 from './Problems/App8';
+import App9 from './Problems/App9';
+import App10 from './Problems/App10';
+import App11 from './Problems/App11';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  //<App1/>
+  //<App2/>
+  //<App3/>
+  //<App4/>
+  //<App5/>
+  //<App6/>
+  //<App7/>
+  //<App8/>
+  //<App9/>
+  //<App10/>
+  <App11/>
+);
+
