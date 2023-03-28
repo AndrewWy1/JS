b07f0914a432dae19b6b3029a4a4fe93d5758906
Andrew Kulchickii <droppet228@gmail.com>
2023-03-28 10:59:30 +0300

    add lab #19

---
+++ lab_19/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App1 from './App1';
+import App2 from './App2';
+import App3 from './App3';
+import App4 from './App4';
+import App5 from './App5';
+import App6 from './App6';
+import App7 from './App7';
+import App8 from './App8';
+import App9 from './App9';
+import App10 from './App10';
+import App11 from './App11';
+import App12 from './App12';
+import App13 from './App13';
+import App14 from './App14';
+import App15 from './App15';
+import App16 from './App16';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  //<App1/>
+  //<App2/>
+  //<App3/>
+  //<App4/>
+  //<App5/>
+  //<App6/>
+  //<App7/>
+  //<App8/>
+  //<App9/>
+  //<App10/>
+  //<App11/>
+  //<App12/>
+  //<App13 />
+  //<App14/>
+  //<App15/>
+  <App16/>
+);
